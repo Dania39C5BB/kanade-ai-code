455f04f15541e300960750e86b9dbf1f1ce4d30d
李自超 <2079253084@qq.com>
2026-06-08 14:24:10 +0800

    初始化项目

---
+++ src/main/java/com/kanade/kanadeaicode/KanadeAiCodeApplication.java
@@ -0,0 +1,13 @@
+package com.kanade.kanadeaicode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanadeAiCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KanadeAiCodeApplication.class, args);
+    }
+
+}
